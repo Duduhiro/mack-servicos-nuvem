3a51f4e6f5f70950636e328568a50a155d48a893
duduhiro0312@gmail.com <duduhiro0312@gmail.com>
2025-03-20 18:06:34 -0300

    feat: Structured components for unit converter page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,43 +1,68 @@
 'use client'
 
-import Image from "next/image";
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { ArrowLeftRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
     
     const [date, setDate] = useState("")
 
-    const handleDate = () => {
-        fetch("http://localhost:25000/date")
-            .then((response) => response.text())
-            .then((data) => setDate(data))
-            .catch((error) => setDate("Error when fetching: " + error));
+    const handleDate = async () => {
+
     }
 
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
+                <h1 className="text-8xl font-semibold">UNIT CONVERTER</h1>
 
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto hover:cursor-pointer"
-                        rel="noopener noreferrer"
-                        onClick={handleDate}
-                    >
-                        Get the time
-                    </a>
-                </div>
-                <div>
-                    {date}
+                <div className="w-full flex gap-12 items-center flex-col">
+                    <div className="w-full flex items-center justify-center">
+                        <Select>
+                            <SelectTrigger className="w-[200px]">
+                                <SelectValue placeholder="Unit..." />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="Length" className="hover:bg-neutral-900">Length</SelectItem>
+                                <SelectItem value="Temperature" className="hover:bg-neutral-900">Temperature</SelectItem>
+                                <SelectItem value="Time" className="hover:bg-neutral-900">Time</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="w-full flex">
+                        <div className="grow-1 flex items-end pr-4 flex-col gap-6">
+                            <Select>
+                                <SelectTrigger className="w-[200px]">
+                                    <SelectValue placeholder="Unit..." />
+                                </SelectTrigger>
+                                <SelectContent className="bg-background">
+                                    <SelectItem value="Length" className="hover:bg-neutral-900">Length</SelectItem>
+                                    <SelectItem value="Temperature" className="hover:bg-neutral-900">Temperature</SelectItem>
+                                    <SelectItem value="Time" className="hover:bg-neutral-900">Time</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <Input type="number" placeholder="Converted..." value={"2000"} className="w-[200px]" disabled/>
+                        </div>
+                        <div>
+                            <Button className="border border-white"><ArrowLeftRight/></Button>
+                        </div>
+                        <div className="grow-1 flex items-start pl-4 flex-col gap-6">
+                            <Select>
+                                <SelectTrigger className="w-[200px]">
+                                    <SelectValue placeholder="Unit..." />
+                                </SelectTrigger>
+                                <SelectContent className="bg-background">
+                                    <SelectItem value="Length" className="hover:bg-neutral-900">Length</SelectItem>
+                                    <SelectItem value="Temperature" className="hover:bg-neutral-900">Temperature</SelectItem>
+                                    <SelectItem value="Time" className="hover:bg-neutral-900">Time</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <Input type="number" placeholder="Converted..." value={"2000"} className="w-[200px]" disabled/>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
